6c7f4e03a79474c77df493722b4fd60ce254b448
iksadNorth <iksadNorth@gmail.com>
2023-08-25 18:32:39 +0900

    Controller 작성 중 오류 수정. # 수정 내용. - ArticleReadRes 필드 추가.

--- src/main/java/com/sparta/blogapp/model/ArticleReadRes.java
+++ src/main/java/com/sparta/blogapp/model/ArticleReadRes.java
@@ -2,8 +2,13 @@ package com.sparta.blogapp.model;
 
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder(toBuilder = true)
 public record ArticleReadRes(
-
+        String title,
+        String author,
+        String content,
+        LocalDateTime createdAt
 ) {
 }
