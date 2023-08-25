b2f3eebbaff1153575625f7b8f8477cc2b047669
iksadNorth <iksadNorth@gmail.com>
2023-08-25 20:44:57 +0900

    Repository 구현 # 생성 내용. - ArticleRepositoryImpl 내용 구현 - build.gradle에 JDBC Starter 추가. - 보안을 위해 application.properties 삭제. - .gitignore에 application.properties 추가.

--- src/main/java/com/sparta/blogapp/entity/Article.java
+++ src/main/java/com/sparta/blogapp/entity/Article.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 @Getter @Setter @NoArgsConstructor
@@ -14,4 +16,23 @@ public class Article {
     private String content;
     private LocalDateTime createdAt;
     private String password;
+
+    public static Article fromResultSet(ResultSet rs, int rowNum) throws SQLException {
+        Article entity = new Article();
+
+        Long id = rs.getLong("id");
+        String title = rs.getString("title");
+        String author = rs.getString("author");
+        String content = rs.getString("content");
+        LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+        String password = rs.getString("password");
+
+        entity.setId(id);
+        entity.setTitle(title);
+        entity.setAuthor(author);
+        entity.setContent(content);
+        entity.setCreatedAt(createdAt);
+        entity.setPassword(password);
+        return entity;
+    }
 }
