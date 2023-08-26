8c3fa9f8cdeb1abe62cecaa2f063e3f05c18d07e
iksadNorth <iksadNorth@gmail.com>
2023-08-26 16:59:01 +0900

    게시물 일괄 조회 시, 내림차순 미적용 버그 수정 # 수정 내용 일괄 조회 시, 내림차순이 적용되지 못해 수정. 수정을 위해 ArticleRepository 인터페이스에서 findAll 메서드에 직접 쿼리를 작성함.

--- src/main/java/com/sparta/blogapp/repository/ArticleRepository.java
+++ src/main/java/com/sparta/blogapp/repository/ArticleRepository.java
@@ -2,6 +2,12 @@ package com.sparta.blogapp.repository;
 
 import com.sparta.blogapp.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    @Query("SELECT a FROM Article a ORDER BY a.createdAt DESC")
+    List<Article> findAll();
 }
